5537609bc09863052ac2695655e838219f5f97e4
Matheus <matheusdmm@users.noreply.github.com>
2025-04-25 18:52:41 -0300

    lisinho

--- src/graphql/typedefs.ts
+++ src/graphql/typedefs.ts
@@ -1,21 +1,39 @@
 import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
-  type Order {
-    id: ID!
-    customer: String!
-    item: String!
-    status: String!
-    created_at: String!
-  }
-
   type Query {
     orders: [Order!]!
   }
 
   type Mutation {
-    createOrder(item: String!): Order!
+    createOrder(input: CreateOrderInput!): Order!
     createUser(email: String!, password: String!): String!
     loginUser(email: String!, password: String!): String!
+  }
+
+  input CreateOrderInput {
+    customer: String!
+    items: [OrderItemInput!]!
+  }
+
+  input OrderItemInput {
+    name: String!
+    quantity: Int!
+    size: String!
+    flavors: [String!]!
+  }
+
+  type Order {
+    id: ID!
+    customer: String!
+    items: [OrderItem!]!
+    status: String!
+  }
+
+  type OrderItem {
+    name: String!
+    quantity: Int!
+    size: String!
+    flavors: [String!]!
   }
 `;
